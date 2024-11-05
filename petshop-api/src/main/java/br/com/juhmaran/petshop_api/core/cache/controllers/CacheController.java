229c42f9ba10e6cf790c5ab7519cc8456caa8515
JulianeMaran32 <julianemaran@gmail.com>
2024-11-05 15:55:13 -0300

    fix: melhorias no controlador do cache

--- petshop-api/src/main/java/br/com/juhmaran/petshop_api/core/cache/controllers/CacheController.java
+++ petshop-api/src/main/java/br/com/juhmaran/petshop_api/core/cache/controllers/CacheController.java
@@ -1,40 +1,33 @@
 package br.com.juhmaran.petshop_api.core.cache.controllers;
 
 import br.com.juhmaran.petshop_api.core.cache.services.CacheService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
-/**
- * Controlador para operações de cache.
- * <p>
- * Este controlador fornece endpoints para interagir com o cache da aplicação.
- * </p>
- * <p>Esta classe utiliza o framework Spring para gerenciar o cache.</p>
- * <p>Responsabilidade: Expor endpoints para operações de cache.</p>
- * <p>Princípio da Responsabilidade Única: Esta classe lida apenas com a exposição de endpoints.</p>
- * <p>As operações de recuperação de entradas de cache foram movidas para a classe CacheService.</p>
- * <p>Isso garante que cada classe tenha uma única responsabilidade.</p>
- *
- * @author Juliane Maran
- */
 @Slf4j
 @RestController
 @RequestMapping("/cache")
 @RequiredArgsConstructor
+@Tag(name = "Cache", description = "Controlador para operações de cache. Este controlador fornece endpoints para interagir com o cache da aplicação.")
 public class CacheController {
 
     private final CacheService cacheService;
 
-    /**
-     * Recupera o conteúdo do cache.
-     *
-     * @return um mapa contendo os nomes dos caches e suas respectivas entradas
-     */
+    @Operation(summary = "Recupera o cache", operationId = "getCacheContents", tags = {"Cache"},
+            description = "Recuperação do conteúdo do cache.")
+    @ApiResponse(responseCode = "500", description = "Erro interno",
+            content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
     @GetMapping("/contents")
     public Map<String, Object> getCacheContents() {
         Map<String, Object> cacheContents = cacheService.getCacheContents();
