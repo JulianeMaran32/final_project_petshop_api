3056ad978bfb4fccdee27df1d3db856296c5b388
JulianeMaran32 <julianemaran@gmail.com>
2024-11-05 15:56:07 -0300

    fix: melhorias adicionadas

--- petshop-api/src/main/java/br/com/juhmaran/petshop_api/PetshopApiApplication.java
+++ petshop-api/src/main/java/br/com/juhmaran/petshop_api/PetshopApiApplication.java
@@ -1,14 +1,32 @@
 package br.com.juhmaran.petshop_api;
 
 import br.com.juhmaran.petshop_api.api.config.FeignConfig;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Petshop API", version = "1.0",
+                description = "API para gerenciamento de petshops.",
+                contact = @Contact(
+                        name = "Juliane Maran",
+                        email = "juliane.vmaran@gmail.com")
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Documentação da API",
+                url = "http://localhost:8080/api/swagger-ui/index.html#/"),
+        servers = {
+                @Server(url = "https://petshop-web-api-a5aeanc2g5gzgzer.eastus2-01.azurewebsites.net/", description = "Produção"),
+                @Server(url = "http://localhost:8080/api", description = "Local")}
+)
 @EnableGlobalAuthentication
 @EnableCaching(proxyTargetClass = true)
 @EnableFeignClients(basePackages = "br.com.juhmaran.petshop_api.api.address.clients", defaultConfiguration = FeignConfig.class)
